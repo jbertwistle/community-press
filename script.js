4d875b22aab5b8071d3896187e72d28206681807
jbertwistle <jbertwistle@gmail.com>
2026-08-02 16:43:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const button = document.getElementById("headlineButton");
+const page = document.getElementById("page");
 
+button.addEventListener("click", () => {
+
+    const headline = document.createElement("h1");
+
+    headline.innerText = "Your Headline";
+
+    headline.style.position = "absolute";
+    headline.style.left = "50px";
+    headline.style.top = "50px";
+
+    page.appendChild(headline);
+
+});
